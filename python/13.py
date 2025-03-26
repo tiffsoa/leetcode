b0f2c2f965a42fd3f36a88e9d187fe0d2450fcdd
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-03-26 12:54:28 -0400

    ywo sum

--- python/13.py
+++ python/13.py
@@ -7,7 +7,7 @@ class Solution(object):
         # largest to smallest: add them up
         # smaller before larger: substract smaller
 
-        roman = { "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        roman = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
         result = 0
 

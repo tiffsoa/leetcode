add2e8a9c5c8d425f677c613be6632b0a9f09d7a
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-04 10:49:12 -0400

    sales by match

---
+++ hacker_rank/sales_by_match.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs = {}
+    count = 0
+    
+    for color in ar:
+        if color in pairs:
+            pairs[color] += 1
+        else:
+            pairs[color] = 1
+    
+    for color in pairs:
+        count += pairs[color] // 2
+        
+    return count              
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
